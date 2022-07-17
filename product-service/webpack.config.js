7afb01acf3cfcdbc2b3e066b5f6505195d1c82dc
Dzianis Dashkevich <Dzianis.Dashkevich@vizio.com>
2022-07-17 15:36:44 -0700

    add service configs

---
+++ product-service/webpack.config.js
@@ -0,0 +1,24 @@
+const { resolve } = require('path');
+const { lib }  = require('serverless-webpack');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+console.log(lib.entries);
+
+module.exports = {
+    mode: lib.webpack.isLocal ? 'development' : 'production',
+    entry: lib.entries,
+    resolve: {
+        extensions: ['.ts'],
+        plugins: [new TsconfigPathsPlugin()]
+    },
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: resolve(__dirname, 'src'),
+            },
+        ],
+    },
+};
