47844336ba2f9ddd3351baad7c40e9e5530e87a0
Dzianis Dashkevich <Dzianis.Dashkevich@vizio.com>
2022-07-25 17:47:51 -0700

    upd tests

--- product-service/test/getProductsList.test.ts
+++ product-service/test/getProductsList.test.ts
@@ -1,14 +1,27 @@
-import getProductList from '@functions/getProductsList';
-import getMockProductsList from '@libs/mock-products-list';
-import {APIGatewayProxyEvent} from 'aws-lambda';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import getProductListFactory from '@functions/getProductsList';
+import { instance, mock, when } from 'ts-mockito';
+import { ProductRepository } from '@services/productService';
 
-describe('getProductsList spec', () => {
+describe('getProductById spec', () => {
+    const mockProductRepository = mock<ProductRepository>();
+    const getProductList = getProductListFactory(instance(mockProductRepository));
+
     it('should return all available products', async () => {
+        const mockProductList = [{
+            description: 'A strong feeling of annoyance, displeasure, or hostility.',
+            id: '7567ec4b-b10c-48c5-9345-fc73c48a80aa',
+            price: 24,
+            title: 'Anger',
+            count: 10,
+        }];
+
+        when(mockProductRepository.getProducts()).thenResolve(mockProductList);
+
         const actual = await getProductList({} as APIGatewayProxyEvent);
-        const expected = await getMockProductsList();
 
         expect(actual).toEqual({
-            body: JSON.stringify(expected),
+            body: JSON.stringify(mockProductList),
             statusCode: 200,
         });
     });
